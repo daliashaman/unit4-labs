34ca76655e9b321fe01ab070f75ba1c503f799b0
“daliashaman” <daliashaman@gmail.com>
2022-11-02 18:47:32 -0400

    Ready, Set, Action Lab

---
+++ script.js
@@ -0,0 +1,24 @@
+function main(){
+    let grow = document.getElementById("grow-me");
+    grow.classList.add("big");
+
+    let shrink = document.getElementById("shrink-me");
+    shrink.classList.remove("big");  
+
+    let list = document.querySelectorAll("li")
+    console.log(list);
+    
+    let link = document.querySelector(".link");
+    link.setAttribute("href", "https://www.example.com");
+    link.innerText ="somewhere";
+
+    let hide = document.getElementById("hide-me");
+    hide.style.display = "none";
+
+    let show = document.getElementById("show-me");
+    show.style.display = "block";
+
+    let input = document.getElementById("name");
+
+}
+
